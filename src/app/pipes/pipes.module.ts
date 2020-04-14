a03a3df45e50ed48a5cb4ce58583427e6d720e24
PEREZ MEZA, GEORGE FELIX <george.perez@santander.cl>
2020-04-14 18:58:13 -0400

    Agregamos archivos ts para version productiva.

--- src/app/pipes/pipes.module.ts
+++ src/app/pipes/pipes.module.ts
@@ -2,24 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 
-import { NgMathPipesModule } from 'angular-pipes';
-import { FloorPipe } from 'angular-pipes';
-
 import { TimeDifferencePipe } from './time-difference.pipe';
 import { TimeAgoPipe } from './time-ago.pipe';
 
 @NgModule({
   imports: [
     CommonModule,
-    IonicModule,
-    NgMathPipesModule
+    IonicModule
   ],
   declarations: [
     TimeDifferencePipe,
     TimeAgoPipe
   ],
   exports: [
-    FloorPipe,
     TimeDifferencePipe,
     TimeAgoPipe
   ],
